2776b2e8c71cf06fff4f430d8a2a2a77d7431b17
WebApp07 <aminezouguig@proton.me>
2024-04-25 03:31:25 -1000

    Sending a sign-up verification email

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -15,6 +16,16 @@ if (!cached) {
     promise: null,
   };
 }
+
+const tranporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
+});
 
 interface Args {
   initOptions?: Partial<InitOptions>;
@@ -33,6 +44,11 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: tranporter,
+        fromAddress: "onboarding@resend.dev",
+        fromName: "DigitalHippo",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
